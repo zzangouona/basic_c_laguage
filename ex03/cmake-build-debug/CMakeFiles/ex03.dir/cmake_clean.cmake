278fb23678dd83f3060aca1013a313c34949f21d
zzanguna <zzanguna>
2020-10-30 02:23:32 +0900

    ex03 폴터 first commit

---
+++ ex03/cmake-build-debug/CMakeFiles/ex03.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex03.dir/main.c.o"
+  "ex03"
+  "ex03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ex03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
