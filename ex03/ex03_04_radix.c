2a26beb60461ade08271344d9b300eabd2d19ac1
zzanguna <zzanguna>
2020-10-31 01:38:45 +0900

    3장 예제 커밋

---
+++ ex03/ex03_04_radix.c
@@ -0,0 +1,11 @@
+//
+// Created by 짱유나 on 2020/10/30.
+//
+
+#include <stdio.h>
+int main(){
+    int a = 014; //8진수
+    int b = 12; //10진수
+    int c = 0xc;//16진수
+    printf("a=%d, b=%d, c=%d",a,b,c);
+}
