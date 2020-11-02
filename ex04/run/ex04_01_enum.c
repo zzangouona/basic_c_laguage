55051add7be2b0c8e50c5449d3b031cb8c9135ae
zzanguna <zzanguna>
2020-11-03 00:39:09 +0900

    ex04 enum 개념 이해와 예제 에러 코드 수정 및 동서남북 배열 표시 함수 추가

--- ex04/run/ex04_01_enum.c
+++ ex04/run/ex04_01_enum.c
@@ -4,15 +4,21 @@
 
 #include <stdio.h>
 
-void move(enum origin to){
-    printf("%d쪽으로 이동한다.\n", to);
+//error 코드이다.
+typedef enum origin { EAST, WEST, SOUTH, NORTH };
+void move(enum origin to)
+{
+    printf("%d쪽으로 이동한다.\n",to);
+}
+void koreanMove(enum origin to){
+    static char *oriname[]  = {"동", "서","남","북"};
+    printf("%s쪽으로 이동한다.\n",oriname[to]);
 }
 
 int main(){
-    enum origin { EAST, WEST, SOUTH, NORTH };
     enum origin mark =  WEST;
     enum origin head = SOUTH;
     move(head);
-
+    koreanMove(mark);
     return 0;
 }
